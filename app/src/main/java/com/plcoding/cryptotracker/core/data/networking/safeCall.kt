f1bd4cdb60cfc232329c784fc6572e829952279b
Fathya Azra Nazmira 039 <108808542+FathyaAzra@users.noreply.github.com>
2025-01-24 06:25:24 +0700

    front screen

--- app/src/main/java/com/plcoding/cryptotracker/core/data/networking/safeCall.kt
+++ app/src/main/java/com/plcoding/cryptotracker/core/data/networking/safeCall.kt
@@ -2,13 +2,14 @@ package com.plcoding.cryptotracker.core.data.networking
 
 import com.plcoding.cryptotracker.core.domain.util.NetworkError
 import com.plcoding.cryptotracker.core.domain.util.Result
+import io.ktor.client.statement.HttpResponse
 import io.ktor.util.network.UnresolvedAddressException
 import kotlinx.coroutines.ensureActive
 import kotlinx.serialization.SerializationException
 import kotlin.coroutines.coroutineContext
 
 suspend inline fun <reified T> safeCall(
-    execute: () -> Unit
+    execute: () -> HttpResponse
 ): Result<T, NetworkError> {
     val response = try {
         execute()
